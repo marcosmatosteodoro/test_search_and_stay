f1cc7e3fb80d20fb0203b650ec0ae54e871c0055
marcosmatosteodoro <mp.matos25@gmail.com>
2023-01-15 18:27:56 -0300

    feat: Validating request

--- app/Http/Controllers/BookStoreController.php
+++ app/Http/Controllers/BookStoreController.php
@@ -10,27 +10,42 @@ class BookStoreController extends Controller
     //
     public function index()
     {
-        return BookStore::all();
+        return response()->json([ "data"=> BookStore::all()], 200); 
     }
 
-    public function store(Request $request){
-        BookStore::create($request->all());
+    public function store(Request $request, BookStore $bookStore){
+
+        $rules = [
+            'name' => 'required|min:3',
+            'ISBN' => 'required|min:5',
+            'value' => 'required|min:3',
+        ];
+
+        $request->validate($rules);
+        
+        $bookStoreData = $request->only('name', 'ISBN', 'value');
+
+        $bookStore->create($bookStoreData);
+
+        return response()->json([ "message"=> "Registration created successfully."], 201); 
     }
 
     public function show($id)
     {
-        return BookStore::findOrFail($id);
+        return response()->json([ "data"=> BookStore::findOrFail($id)], 200); 
     }
 
     public function update(Request $request, $id)
     {
         $user = BookStore::findOrFail($id);
         $user->update($request->all());
+        return response()->json([ "message"=> "Registration updated successfully."], 200); 
     }
 
     public function destroy($id)
     {
         $user = BookStore::findOrFail($id);
         $user->delete();
+        return response()->json([ "message"=> "Registration deleted successfully."], 200); 
     }
 }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,27 +10,34 @@ class UserController extends Controller
     //
     public function index()
     {
-        return User::all();
+        return response()->json([ "data"=> User::all()], 200); 
     }
 
-    public function store(Request $request){
-        User::create($request->all());
+    public function store(Request $request, User $user){
+        $userData = $request->only('name', 'email', 'password');
+        $userData['password'] = bcrypt($userData['password']);
+        
+        $user->create($userData);
+        return response()->json([ "message"=> "User created successfully."], 201); 
     }
 
     public function show($id)
     {
-        return User::findOrFail($id);
+        return response()->json([ "data"=> User::findOrFail($id)], 200); 
+        
     }
 
     public function update(Request $request, $id)
     {
         $user = User::findOrFail($id);
         $user->update($request->all());
+        return response()->json([ "message"=> "User updated successfully."], 200); 
     }
 
     public function destroy($id)
     {
         $user = User::findOrFail($id);
         $user->delete();
+        return response()->json([ "message"=> "User deleted successfully."], 200); 
     }
 }
